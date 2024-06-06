8410a1d5f307b6cf40ed110db4842d42a6d9dcd0
Michael Chase <themikechase@gmail.com>
2024-06-06 13:22:45 -0700

    remvoing about

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,7 +4,6 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Use Cases' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
