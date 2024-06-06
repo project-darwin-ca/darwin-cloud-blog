4553ebcc0a598fb71eff1fecf0e6dcafbb10b131
Michael Chase <themikechase@gmail.com>
2024-06-06 12:08:33 -0700

    polymer design addition

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,13 +9,13 @@ const projectsData: Project[] = [
   {
     title: 'Polymer Design',
     description: `Using Darwin, researchers can design and optimize new polymers with desired mechanical, thermal, and chemical properties. By simulating polymer behavior at the molecular level, Darwin aids in predicting performance and identifying promising candidates for various applications.`,
-    imgSrc: '/static/images/polymer-design.png',
-    href: 'https://www.google.com',
+    imgSrc: '/static/images/polymerdesign.webp',
+    href: '/blog/the-time-machine',
   },
   {
     title: 'Battery Materials Innovation',
     description: `Darwin's platform enables the development of advanced battery materials by simulating electrochemical properties and stability. This accelerates the discovery of materials with higher energy densities and longer lifespans, crucial for next-generation batteries in electric vehicles and renewable energy storage.`,
-    imgSrc: '/static/images/battery-materials.png',
+    imgSrc: '/static/images/battery-design.webp',
     href: '/blog/the-time-machine',
   },
 ]
