617422ded5a129e4364c0985c8d248c90f4baf94
Michael Chase <themikechase@gmail.com>
2024-06-06 12:14:41 -0700

    forensic

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -18,6 +18,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/battery-design.webp',
     href: '/blog/the-time-machine',
   },
+  {
+    title: 'Forensic Identification',
+    description: `Darwin's platform, in combination with NMR Spectroscopy, plays a critical role in forensic science for the identification of unknown chemical compounds. By offering detailed molecular simulations and precise analysis, Darwin enhances the accuracy and efficiency of forensic investigations, aiding in the swift identification of substances in criminal cases and toxicology studies.`,
+    imgSrc: '/static/images/forensic-identification.webp',
+    href: '/blog/the-time-machine',
+  },
 ]
 
 export default projectsData
