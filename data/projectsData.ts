7a302743cf20366e27785223a73c17988fa9a08b
Michael Chase <themikechase@gmail.com>
2024-06-11 10:33:09 -0700

    removig paths

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -16,19 +16,19 @@ const projectsData: Project[] = [
     title: 'Drug Discovery and Development',
     description: `Leveraging Darwin's advanced computational chemistry capabilities, researchers can accelerate drug discovery and development. By simulating drug-target interactions at the molecular level, Darwin aids in identifying potential drug candidates, optimizing lead compounds, and predicting pharmacokinetic and toxicological properties, thus streamlining the drug development process.`,
     imgSrc: '/static/images/psivant.webp',
-    href: '/blog/the-time-machine',
+    href: '',
   },
   {
     title: 'Battery Material Design',
     description: `Darwin's platform enables the development of advanced battery materials by simulating electrochemical properties and stability. This accelerates the discovery of materials with higher energy densities and longer lifespans, crucial for next-generation batteries in electric vehicles and renewable energy storage.`,
     imgSrc: '/static/images/battery-design.webp',
-    href: '/blog/the-time-machine',
+    href: '',
   },
   {
     title: 'Forensic Identification',
     description: `Darwin's platform, in combination with NMR Spectroscopy, plays a critical role in forensic science for the identification of unknown chemical compounds. By offering detailed molecular simulations and precise analysis, Darwin enhances the accuracy and efficiency of forensic investigations, aiding in the swift identification of substances in criminal cases and toxicology studies.`,
     imgSrc: '/static/images/forensic_identification.jpeg',
-    href: '/blog/the-time-machine',
+    href: '',
   },
 ]
 
