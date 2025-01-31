ea303a2ac921d843d2ba3f9f9a32f60d4f401026
Mike Chase <themikechase@gmail.com>
2025-01-30 21:03:49 -0800

    orib

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: 'https://www.darwincloud.xyz/', title: 'Products' },
+  { href: 'https://www.orbitsky.xyz/', title: 'Products' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Use Cases' },
